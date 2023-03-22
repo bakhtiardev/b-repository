2fe8a5916b18db4308496c7ca407e893ff1cdd10
Bakhtiar <bakhtiarhussain.bcsf18@iba-suk.edu.pk>
2023-03-22 20:14:09 +0500

    map added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+} from 'https://cdn.esm.sh/react-leaflet';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
